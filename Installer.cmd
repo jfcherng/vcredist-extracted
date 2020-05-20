fa6cc94550323edb56f801cad61c65537c7c5215
Jack Cherng <jfcherng@gmail.com>
2020-05-20 11:04:32 +0800

    v0.30.0
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- Installer.cmd
+++ Installer.cmd
@@ -1,7 +1,7 @@
 @setlocal DisableDelayedExpansion
 @echo off
 set _debug=0
-set vci=v0.29.0
+set vci=v0.30.0
 set auto=0
 set verbosity=/quiet
 set verbosityshort=/qn /norestart
@@ -134,7 +134,7 @@ set "_version09=307297523"
 set "_version10=40219473"
 set "_version11=61135400"
 set "_version12=406640"
-set "_version14=26287203"
+set "_version14=27288230"
 set "_vervstor=608280"
 set "_filevstor=%CommonProgramFiles%\Microsoft Shared\VSTO\vstoee.dll"
 
@@ -167,8 +167,8 @@ set "_x86code11m={BD95A8CD-1D9F-35AD-981A-3E7925026EBB}"
 set "_x86code11a={B175520C-86A2-35A7-8619-86DC379688B9}"
 set "_x86code12m={8122DAB1-ED4D-3676-BB0A-CA368196543E}"
 set "_x86code12a={D401961D-3A20-3AC7-943B-6139D5BD490A}"
-set "_x86code14m={31C9EB3A-5F0C-49E7-8E6C-D404E48F433D}"
-set "_x86code14a={2F69FB2B-2C48-491C-B249-22C1BDCE1117}"
+set "_x86code14m={D1D81698-AF9C-41A9-BF21-50FC65D8100B}"
+set "_x86code14a={004BE856-CADE-4DAF-ACF2-DCF8E6CF88F8}"
 
 set "_x64code08={ad8a2fa1-06e7-4b0d-927d-6e54b3d31028}"
 set "_x64code09={5FCE6D76-F5DC-37AB-B2B8-22AB8CEDB1D4}"
@@ -178,8 +178,8 @@ set "_x64code11m={CF2BEA3C-26EA-32F8-AA9B-331F7E34BA97}"
 set "_x64code11a={37B8F9C7-03FB-3253-8781-2517C99D7C00}"
 set "_x64code12m={53CF6934-A98D-3D84-9146-FC4EDF3D5641}"
 set "_x64code12a={010792BA-551A-3AC0-A7EF-0FAB4156C382}"
-set "_x64code14m={DD1EC0FD-3F0A-4740-A05E-1DCD14A6B0D1}"
-set "_x64code14a={CB4A0FDE-1126-4AE2-97C6-A243692C3D95}"
+set "_x64code14m={5E32FCCA-C3AF-48A3-85A7-A6EF70C58577}"
+set "_x64code14a={7777D775-6BD3-4EEC-A4F8-A10F0E8CE93B}"
 
 if exist "!_temp!\msi.txt" del /f /q "!_temp!\msi.txt"
 if exist "!_temp!\wix.txt" del /f /q "!_temp!\wix.txt"
