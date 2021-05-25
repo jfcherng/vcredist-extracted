a7de406e66f00c2855cd265bf70c4e2cbb185683
Jack Cherng <jfcherng@gmail.com>
2021-05-26 07:24:56 +0800

    v0.49.0
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- Installer.cmd
+++ Installer.cmd
@@ -1,7 +1,7 @@
 @setlocal DisableDelayedExpansion
 @echo off
 set _debug=0
-set vci=v0.48.0
+set vci=v0.49.0
 set auto=0
 set verbosity=/quiet
 set verbosityshort=/qn /norestart
@@ -142,23 +142,23 @@ set "_ver09=307297523"
 set "_ver10=40219473"
 set "_ver11=61135400"
 set "_ver12=406640"
-set "_ver14=29301291"
+set "_ver14=29301293"
 
 set "_filevstor=%CommonProgramFiles%\Microsoft Shared\VSTO\vstoee.dll"
 
-set "_x86file08=%SystemRoot%\WinSxS\x86_microsoft.vc80.mfc_1fc8b3b9a1e18e3b_8.0.50727.6229_none_cbee8c4a4710d003\mfcm80.dll"
-set "_x86file09=%SystemRoot%\WinSxS\x86_microsoft.vc90.mfc_1fc8b3b9a1e18e3b_9.0.30729.7523_none_4bf3b680bf9ea69d\mfcm90.dll"
-set "_x86file10=mfcm100.dll"
-set "_x86file11=mfcm110.dll"
-set "_x86file12=mfcm120.dll"
-set "_x86file14=mfcm140.dll"
+set "_x86file08=%SystemRoot%\WinSxS\x86_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.6229_none_d089f796442de10e\msvcp80.dll"
+set "_x86file09=%SystemRoot%\WinSxS\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.7523_none_508f21ccbcbbb7a8\msvcp90.dll"
+set "_x86file10=msvcp100.dll"
+set "_x86file11=msvcp110.dll"
+set "_x86file12=msvcp120.dll"
+set "_x86file14=msvcp140.dll"
 
-set "_x64file08=%SystemRoot%\WinSxS\amd64_microsoft.vc80.mfc_1fc8b3b9a1e18e3b_8.0.50727.6229_none_844155733294a6fd\mfcm80.dll"
-set "_x64file09=%SystemRoot%\WinSxS\amd64_microsoft.vc90.mfc_1fc8b3b9a1e18e3b_9.0.30729.7523_none_04467fa9ab227d97\mfcm90.dll"
-set "_x64file10=mfcm100.dll"
-set "_x64file11=mfcm110.dll"
-set "_x64file12=mfcm120.dll"
-set "_x64file14=mfcm140.dll"
+set "_x64file08=%SystemRoot%\WinSxS\amd64_microsoft.vc80.crt_1fc8b3b9a1e18e3b_8.0.50727.6229_none_88dcc0bf2fb1b808\msvcp80.dll"
+set "_x64file09=%SystemRoot%\WinSxS\amd64_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.7523_none_08e1eaf5a83f8ea2\msvcp90.dll"
+set "_x64file10=msvcp100.dll"
+set "_x64file11=msvcp110.dll"
+set "_x64file12=msvcp120.dll"
+set "_x64file14=msvcp140.dll"
 
 set "_x86code08={710f4c1c-cc18-4c49-8cbf-51240c89a1a2}"
 set "_x86code09={9BE518E6-ECC6-35A9-88E4-87755C07200F}"
@@ -168,8 +168,8 @@ set "_x86code11m={BD95A8CD-1D9F-35AD-981A-3E7925026EBB}"
 set "_x86code11a={B175520C-86A2-35A7-8619-86DC379688B9}"
 set "_x86code12m={8122DAB1-ED4D-3676-BB0A-CA368196543E}"
 set "_x86code12a={D401961D-3A20-3AC7-943B-6139D5BD490A}"
-set "_x86code14m={829D861E-5F49-4B4C-BB2D-89CB098C183C}"
-set "_x86code14a={59F00D58-CCD4-480D-BAEA-C78A4E4A08C1}"
+set "_x86code14m={664F72A5-21E2-427A-B78D-3BDE9A877E5B}"
+set "_x86code14a={8F31BD8A-96F6-4576-8282-C6F1886F9F8E}"
 
 set "_x64code08={ad8a2fa1-06e7-4b0d-927d-6e54b3d31028}"
 set "_x64code09={5FCE6D76-F5DC-37AB-B2B8-22AB8CEDB1D4}"
@@ -179,8 +179,8 @@ set "_x64code11m={CF2BEA3C-26EA-32F8-AA9B-331F7E34BA97}"
 set "_x64code11a={37B8F9C7-03FB-3253-8781-2517C99D7C00}"
 set "_x64code12m={53CF6934-A98D-3D84-9146-FC4EDF3D5641}"
 set "_x64code12a={010792BA-551A-3AC0-A7EF-0FAB4156C382}"
-set "_x64code14m={1D035C5A-5934-41DD-9FC9-01B3148B3EE7}"
-set "_x64code14a={DDEB3CA3-C299-4A8D-9D8A-286BCBEFEAB6}"
+set "_x64code14m={7397283E-6C2F-410C-A003-C8E5E6B41D6B}"
+set "_x64code14a={5E05D42B-B363-4435-B428-D615047EDA54}"
 
 if exist "!_temp!\msi.txt" del /f /q "!_temp!\msi.txt"
 if exist "!_temp!\wix.txt" del /f /q "!_temp!\wix.txt"
